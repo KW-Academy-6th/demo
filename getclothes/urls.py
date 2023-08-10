dd73d349244f2bae7524f38995e277badcf0f3c4
figmarigold115 <fgmrgld115@gmail.com>
2023-08-10 23:46:44 +0900

    DB수정

--- getclothes/urls.py
+++ getclothes/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('home', views.home, name="home"),
     path('list', views.list, name="list"),
     path('list_db', views.list_db, name="list_db"),
-    path('recommend', views.recommend, name="recommend")
+    path('recommend', views.recommend, name="recommend"),
+    path('re_home', views.re_home, name="re_home"),
+    path('list_edit', views.list_edit, name="list_edit"),
+    path('update', views.update, name="update")
 ]
--- getclothes/views.py
+++ getclothes/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from getclothes.models import GC
 
 def home(request):
-    submit_count = request.session.get('submit_count', 0)
     success = False
 
     row_count = GC.get_row_count()
@@ -36,7 +35,6 @@ def list_db(request):
 
     for item in rsGC:
         temp_list = []
-        temp_list.append(item.date)
         temp_list.append(item.top)
         temp_list.append(item.bottom)
         temp_list.append(item.vehicle)
@@ -49,3 +47,54 @@ def list_db(request):
 
 def recommend(request):
     return render(request, 'recommend.html')
+
+def re_home(request):
+    success = False
+
+    if request.method == 'POST':
+        date = request.POST.get('date')
+        top = request.POST.get('top')
+        bottom = request.POST.get('bottom')
+        vehicle = request.POST.get('vehicle')
+        inout = request.POST.get('inout')
+
+        gc_obj = GC.objects.create(date=date, top=top, bottom=bottom, vehicle=vehicle, inout=inout)
+
+        success = True
+
+    return render(request, 're_home.html', {'success': success})
+
+def list_edit(request):
+    date = request.POST.get('date')
+    rsDetail = GC.objects.filter(date=date)
+
+    return render(request, "list_edit.html", {
+        'rsDetail': rsDetail
+    })
+
+def update(request):
+    if request.method == 'POST':
+        date = request.POST.get('date')
+        top = request.POST.get('top')
+        bottom = request.POST.get('bottom')
+        vehicle = request.POST.get('vehicle')
+        inout = request.POST.get('inout')
+
+    try:
+        gc = GC.objects.get(date=date)
+        if top != "":
+            gc.top = top
+        if bottom != "":
+            gc.b_note = bottom
+        if vehicle != "":
+            gc.vehicle = vehicle
+        if inout != "":
+            gc.inout = inout
+
+        gc.save()
+        return redirect('/list')
+    except GC.DoesNotExist:
+        # 원하는 날짜의 데이터가 없을 경우에 대한 처리
+        pass
+
+    return render(request, 'list_edit.html')
